f673b37fd1b3e80df91760a059aad06443b0f98a
ARMartinelli <alberto.riccardo.martinelli@gmail.com>
2022-08-25 21:34:30 +0200

    added dark theme

--- config.h
+++ config.h
@@ -106,32 +106,28 @@ float alpha = 0.8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+  [0] = "#161b1d", /* black   */
+  [1] = "#d22d72", /* red     */
+  [2] = "#568c3b", /* green   */
+  [3] = "#8a8a0f", /* yellow  */
+  [4] = "#257fad", /* blue    */
+  [5] = "#5d5db1", /* magenta */
+  [6] = "#2d8f6f", /* cyan    */
+  [7] = "#7ea2b4", /* white   */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 bright colors */
+  [8]  = "#5a7b8c", /* black   */
+  [9]  = "#d22d72", /* red     */
+  [10] = "#568c3b", /* green   */
+  [11] = "#8a8a0f", /* yellow  */
+  [12] = "#257fad", /* blue    */
+  [13] = "#5d5db1", /* magenta */
+  [14] = "#2d8f6f", /* cyan    */
+  [15] = "#ebf8ff", /* white   */
 
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90",
-	"black", /* default background colour */
+  /* special colors */
+  [256] = "#161b1d", /* background */
+  [257] = "#7ea2b4", /* foreground */
 };
 
 
@@ -139,9 +135,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
 
 /*
